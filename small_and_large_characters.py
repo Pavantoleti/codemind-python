71d52aa1a34be7d93a67212c1ffc0b086f29f90f
Pavantoleti <97424427+Pavantoleti@users.noreply.github.com>
2022-10-16 09:55:46 +0530

    s=input().split() for i in s:     l=[]     for j in i:         l.append(ord(j))     print(chr(min(l)),chr(max(l)),end=' ')

--- small_and_large_characters.py
+++ small_and_large_characters.py
@@ -1,4 +1,6 @@
-s=input()
-l=list(s.split(" "))
-for i in l:
-    print(min(i),max(i),end=" ")
+s=input().split()
+for i in s:
+    l=[]
+    for j in i:
+        l.append(ord(j))
+    print(chr(min(l)),chr(max(l)),end=' ')
