8adbe0cc87dadeed34fd208ceeed838d9e5bf747
Pavantoleti <97424427+Pavantoleti@users.noreply.github.com>
2022-10-16 16:58:45 +0530

    import math n=int(input()) count=0 for i in range(2,int(math.sqrt(n))+1):     if n%i==0:         count+=1 if count==0:     print('prime') else:     print('not a prime')

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,10 @@
+import math
 n=int(input())
-c=0
-for i in range(1,n+1):
+count=0
+for i in range(2,int(math.sqrt(n))+1):
     if n%i==0:
-        c+=1
-if c==2:
+        count+=1
+if count==0:
     print('prime')
 else:
     print('not a prime')
