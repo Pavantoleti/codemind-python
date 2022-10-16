d95ad5ed7ddc4887c1220e9cea4cda0cd1778100
Pavantoleti <97424427+Pavantoleti@users.noreply.github.com>
2022-10-16 09:53:22 +0530

    x=input() l=[] for i in x.split():     l=[]     i=list(i)     for j in i:         a=ord(j)         l.append(a)     print(abs(max(l)-min(l)),end=' ')

--- absolute_difference_of_small_and_large.py
+++ absolute_difference_of_small_and_large.py
@@ -1,7 +1,9 @@
-s=input()
-mn,mx=0,0
-l=list(s.split(" "))
-for i in l:
-    mn=ord(min(i))
-    mx=ord(max(i))
-    print(abs(mx-mn),end=" ")
+x=input()
+l=[]
+for i in x.split():
+    l=[]
+    i=list(i)
+    for j in i:
+        a=ord(j)
+        l.append(a)
+    print(abs(max(l)-min(l)),end=' ')
