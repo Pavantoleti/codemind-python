2b38d0b0c2b34c7758555676833bf227ced41882
Pavantoleti <97424427+Pavantoleti@users.noreply.github.com>
2022-10-16 10:13:36 +0530

    s=input() ch=input() l=[] res=[] vowel=set("aeiouAEIOU") for i in range(0,len(s)):     if s[i] in vowel:         if s[i]==ch:             print(True)             print(i)             break
    else:
        print(False)

--- vowels_in_a_string.py
+++ vowels_in_a_string.py
@@ -1,7 +1,14 @@
 s=input()
-e=input()
-if e in s:
-    print(True)
-    print(s.index(e))
+ch=input()
+l=[]
+res=[]
+vowel=set("aeiouAEIOU")
+for i in range(0,len(s)):
+    if s[i] in vowel:
+        if s[i]==ch:
+            print(True)
+            print(i)
+            break
+            
 else:
     print(False)
