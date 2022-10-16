d194549df71283c47a0ddeabcce3daa7ff9e0462
Pavantoleti <97424427+Pavantoleti@users.noreply.github.com>
2022-10-16 09:29:32 +0530

    n=input().lower().split() n="".join(n) c=0 for i in n:     if(n.count(i)<=1):         c+=1 print(c)

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,7 +1,7 @@
-s=input()
-s=s.lower()
-k=[]
-for i in s:
-    if i not in k and s.count(i)==1 and i!=" ":
-        k.append(i)
-print(len(k))
+n=input().lower().split()
+n="".join(n)
+c=0
+for i in n:
+    if(n.count(i)<=1):
+        c+=1
+print(c)
