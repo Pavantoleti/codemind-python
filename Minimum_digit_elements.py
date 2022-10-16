634bb1fec0a9bd5e724feedf8931c3371b3165d4
Pavantoleti <97424427+Pavantoleti@users.noreply.github.com>
2022-10-16 17:18:48 +0530

    n=int(input()) a=list(map(int,input().split())) m=min(a) c=0 for i in a:     if len(str(m))==len(str(i)):         c+=1 print(c)

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,15 +1,8 @@
-def dig(n):
-    if n==0:
-        return 1
-    c=0
-    while(n!=0):
-        n=n//10
-        c+=1
-    return c
 n=int(input())
-l=[]
 a=list(map(int,input().split()))
-for i in range(n):
-    l.append(dig(a[i]))
-x=min(l)
-print(l.count(x))
+m=min(a)
+c=0
+for i in a:
+    if len(str(m))==len(str(i)):
+        c+=1
+print(c)
